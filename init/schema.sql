86ee957ee4dd399f9a613983826ddd2859ade83d
Henrique Makita <henrique_hmk@yahoo.com>
2025-09-01 21:13:52 -0300

    config: schema sqlite and db connection hadler

---
+++ init/schema.sql
@@ -0,0 +1,14 @@
+CREATE TABLE IF NOT EXISTS 'users'(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username TEXT NOT NULL,
+    password TEXT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS 'orders'(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    product TEXT NOT NULL,
+    description TEXT NOT NULL,
+    order_date DATE NOT NULL,
+    user_id INTEGER NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
